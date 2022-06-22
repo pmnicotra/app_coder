483075f3564312584f973859599e624bd48944e1
pmnicotra <pmnicotra@gmail.com>
2022-06-22 19:51:43 -0300

    app_coder_final

--- app_coder/urls.py
+++ app_coder/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
 from app_coder import views
+from app_coder.views import curso_formulario
+from app_coder.views import prof_formulario
+from app_coder.views import buscar
 
 urlpatterns = [
    
-    path('', views.inicio), #esta era nuestra primer view
+    path('', views.inicio, name="Inicio"), #esta era nuestra primer view
     path('cursos', views.cursos, name="Cursos"),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),
+    path('profesores', views.profesores, name="Profesores"),
+    path('estudiantes', views.estudiantes, name="Estudiantes"),
+    path('entregables', views.entregables, name="Entregables"),
+    path('curso_formulario', views.curso_formulario, name="Curso_Formulario"),
+    path('profesor_formulario', views.prof_formulario, name="Profe_Formulario"),
+    path('buscar/', views.buscar),
 ]
 
--- app_coder/views.py
+++ app_coder/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from app_coder.models import Curso
+from app_coder.forms import Curso_Formulario, Prof_Formulario
+from app_coder.models import Curso, Profesor
+
 # Create your views here.
 
 def curso(self):
@@ -33,3 +35,66 @@ def estudiantes(request):
 def entregables(request):
 
     return render(request, "app_coder/entregables.html")
+
+def curso_formulario(request):
+
+    if request.method == 'POST':
+
+        mi_formulario = Curso_Formulario(request.POST)  #aquí mellega toda la información del html
+
+
+        print(mi_formulario)
+
+        if mi_formulario.is_valid:  #Si pasó la validación de Django
+
+            informacion = mi_formulario.cleaned_data
+
+            curso = Curso(nombre=informacion['curso'], camada=informacion['camada'])
+            
+            curso.save()
+
+            return render(request, 'app_coder/inicio.html') #Vuelvo al inicio o a donde quieran
+
+
+    else:
+        mi_formulario = Curso_Formulario()  #Formulario vacio para construir el html
+
+    return render(request, 'app_coder/curso_formulario.html', {'mi_formulario':mi_formulario})
+
+def prof_formulario(request):
+
+    if request.method == "POST":
+
+        mi_formulario = Prof_Formulario(request.POST)
+        print(mi_formulario)
+
+        if mi_formulario.is_valid:
+
+            informacion = mi_formulario.cleaned_data
+            profesor = Profesor(nombre=informacion['nombre'], apellido=informacion['apellido'], email=informacion['email'], profesion=informacion['profesion'])
+            profesor.save()
+
+            return render(request, 'app_coder/inicio.html')
+
+    else:
+        mi_formulario = Prof_Formulario()
+
+    return render(request, 'app_coder/profesor_formulario.html', {'mi_formulario':mi_formulario})
+
+def buscar(request):
+
+      if request.GET["camada"]: #if request.method == 'Get':
+
+	      #respuesta = f"Estoy buscando la camada nro: {request.GET['camada'] }" 
+            camada = request.GET['camada'] 
+            print(camada)
+            cursos = Curso.objects.filter(camada__icontains=camada)
+            print(cursos)
+            return render(request, "app_coder/inicio.html", {"cursos":cursos, "camada":camada})
+
+      else: 
+
+	      respuesta = "No enviaste datos"
+
+      return render(request,"app_coder/inicio.html", {"respuesta":respuesta})
+        
